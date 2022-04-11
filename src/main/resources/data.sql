b3c00199006a52033f6fba23db78a4b4d78a3f38
Anderson Gonçalves <77304321+Andy-kunn@users.noreply.github.com>
2022-04-11 12:44:06 -0300

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO PESSOA VALUES ('Pa',1,'andersonfilho@discente.ufg.br',
-						   'Anderson','senha', 'Gonçalves Alves Cunha Filho')
-INSERT INTO ENDERECO VALUES (1,'Jardim América','74255180','Goiânia'
-							 'Perto do CEPAL do Jardim América','Goiás',
-							 'Rua c-187',180);
-INSERT INTO PACIENTE (1, 1)
